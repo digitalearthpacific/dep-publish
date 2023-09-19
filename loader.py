8685b46aebf02d05e1590b161a7d85d65960cdf0
Sachindra Singh <sachindras@spc.int>
2023-09-20 11:48:17 +1200

    Encoding Fix.

--- loader.py
+++ loader.py
@@ -5,19 +5,18 @@ account_url = "https://deppcpublicstorage.blob.core.windows.net/output?sv=2022-1
 container_client = ContainerClient.from_container_url(account_url)
 
 #product = "wofs"
-#product_path = "wofs/0.0.1/wofs/2022/"
 #product_path = "dep_ls_wofs/0-0-3/"
 product = "mangroves"
 product_path = "dep_s2_mangroves/0-0-2/"
 
-f = open("loader.sh", "w")
+f = open("loader.sh", "w", encoding="utf-8")
 blob_list = container_client.list_blobs()
 for blob in blob_list:
     fn = blob.name
     if fn.startswith(product_path):
       if fn.endswith(".tif"):
         cmd = 'curl -u admin:geoserver -XPOST -H "Content-type: text/plain" --write-out %{http_code} -d "https://deppcpublicstorage.blob.core.windows.net/output/' + fn + '" "http://localhost:8080/geoserver/rest/workspaces/dep/coveragestores/' + product + '/remote.imagemosaic"'
-        print(cmd)
-        f.write(cmd + "\r\n")
+        print(cmd + "\n\n")
+        f.write(cmd + "\n\n")
 f.close()
 print("Finished.")
